19553398ef41a7a7979d0da70a25f4a351982031
fyInALT <fy@altresear.ch>
2023-08-10 20:56:36 +0800

    impl osp proof exec from accel-wasmi risc0 branch

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,7 +1,11 @@
-use bonsai_starter_methods::{FIBONACCI_ELF, FIBONACCI_ID};
+use bonsai_starter_methods::{OSP_PROOF_ELF, OSP_PROOF_ID};
 use clap::Parser;
-use ethabi::{ethereum_types::U256, ParamType, Token};
+use ethabi::{ethereum_types::{H256, U256}, ParamType, Token};
 use risc0_zkvm::{default_executor_from_elf, ExecutorEnv};
+
+mod osp;
+mod raw;
+use osp::*;
 
 /// Args for prove
 #[derive(Parser, Debug)]
@@ -19,16 +23,12 @@ fn main() {
         .filter_level(log::LevelFilter::Debug)
         .try_init();
 
-    let args = Args::parse();
+    let _args = Args::parse();
 
-    // Encode the arguments
-    let input = ethabi::encode(&[Token::Uint(U256::from(args.n))]);
-
-    // First, we construct an executor environment
-    let env = ExecutorEnv::builder().add_input(&input).build().unwrap();
+    let env = create_env().expect("create env failed");
 
     // Next, we make an executor, loading the (renamed) ELF binary.
-    let mut exec = default_executor_from_elf(env, FIBONACCI_ELF).unwrap();
+    let mut exec = default_executor_from_elf(env, OSP_PROOF_ELF).unwrap();
 
     // Run the executor to produce a session.
     let session = exec.run().unwrap();
@@ -38,15 +38,17 @@ fn main() {
 
     // Optional: Verify receipt to confirm that recipients will also be able to
     // verify your receipt
-    receipt.verify(FIBONACCI_ID).unwrap();
+    receipt.verify(OSP_PROOF_ID).unwrap();
 
     log::trace!("receipt {:?}", receipt);
 
-    let output = ethabi::decode_whole(
-        &[ParamType::Uint(256), ParamType::Uint(256)],
-        &receipt.journal,
-    )
-    .expect("decode journal failed");
+    // let output = ethabi::decode_whole(
+    //    &[ParamType::Uint(256), ParamType::Uint(256)],
+    //    &receipt.journal,
+    //)
+    //.expect("decode journal failed");
 
-    log::info!("journal result: {:?}", output);
+    let post_hash = H256::from_slice(&receipt.journal);
+
+    log::info!("journal result: {:?}", post_hash);
 }
